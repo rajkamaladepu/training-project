140f7ef22231b7745c93f2d20f93fdbfb217cf0b
Raj Adepu <>
2024-05-13 12:27:36 -0700

    Pushing JCR vs AEM

--- core/src/main/java/com/aem/training/site/core/servlets/GetJSONServlet.java
+++ core/src/main/java/com/aem/training/site/core/servlets/GetJSONServlet.java
@@ -2,12 +2,18 @@ package com.aem.training.site.core.servlets;
 
 import java.io.IOException;
 
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
@@ -18,6 +24,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.aem.training.site.core.services.ApplicationService;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
@@ -41,16 +49,46 @@ public class GetJSONServlet extends SlingAllMethodsServlet {
 		String parentPath = request.getParameter("parentPath");
 		String searchTerm = request.getParameter("q");
 		String sortBy = request.getParameter("sortBy");
-
+		
+		ResourceResolver resourceResolver = request.getResourceResolver();
+		Resource pagePath = resourceResolver.getResource(parentPath);
+		Resource jcrContent = pagePath.getChild("jcr:content");
+		
+		//JCR
+		String pageNameFromJCR = "";
+		try {
+			Node jcrContentNode = jcrContent.adaptTo(Node.class);
+			pageNameFromJCR = jcrContentNode.getProperty("jcr:title").getValue().toString();
+		} catch (PathNotFoundException e) {
+			//log
+			e.printStackTrace();
+		} catch (RepositoryException e) {
+			//log
+			e.printStackTrace();
+		}
+		
+		
+		//Sling
+		ValueMap vm = jcrContent.getValueMap();
+		String pageNameFromSling = vm.get("jcr:title", String.class);
+		
+		
+		//AEM 
+		PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+		Page page = pageManager.getPage(parentPath);
+		String pageNameFromAEM = page.getTitle();
 
 		JsonArray tilesArray = new JsonArray();
 		JsonObject test = new JsonObject();
 		test.addProperty("parentPath", StringUtils.isNotBlank(parentPath) ? parentPath : "Parent Path is Empty");
+		test.addProperty("title from JCR API", pageNameFromJCR);
+		test.addProperty("title from Sling API", pageNameFromSling);
+		test.addProperty("title from AEM API", pageNameFromAEM);
+		test.addProperty("externalLink", applicationService.externalizeLink(resourceResolver, parentPath));
 		test.addProperty("searchTerm", StringUtils.isNotBlank(searchTerm) ? searchTerm : "searchTerm is Empty");
 		test.addProperty("sortBy", StringUtils.isNotBlank(sortBy) ? sortBy : "sortBy is Empty");
 		test.addProperty("env name", applicationService.getEnvironmentName());
 		test.addProperty("instance", applicationService.getEnvironmentName());
-		test.addProperty("externalLink", applicationService.externalizeLink(request.getResourceResolver(), parentPath));
 		tilesArray.add(test);
 		
 		
